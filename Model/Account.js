c6c1dad43adbb1f78d0b0abd3fa6153a60f5fb30
nguyen luan <nhungngyen.778396@gmail.com>
2024-03-25 11:11:19 +0700

    doAnTotNghiep

---
+++ Model/Account.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+const AccountSchema = new Schema({
+    username: { type: String, required: true, unique: true }, // Thêm kiểu dữ liệu, bắt buộc, và duy nhất
+    password: { type: String, required: true }
+}, {
+    collection: 'account'
+});
+
+const AccountModel = mongoose.model('account', AccountSchema);
+
+module.exports = AccountModel;
